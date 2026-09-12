00b3947c743881e3d889bcee58828cf5927c868d
akaletekoffilevis <l.akalete20@gmail.com>
2026-09-12 17:05:40 +0100

    feat(firebase): connect apps to fscamp-app and generate options file

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // firebase_options.dart sera ajouté après `flutterfire configure`.
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const QuickEatApp());
 }
